84d16b78c1b3c10cca246a054a02c8da94c5fb65
Sathwick Reddy, Yalla <sathwickreddyy@outlook.com>
2024-08-29 15:09:51 +0530

    React Inception: Basics

---
+++ app.js
@@ -0,0 +1,17 @@
+const heading = React.createElement("h1", {id: "heading", xyz:"abc"}, "HelloWorld From React! ");
+
+console.log(heading);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const h1 = React.createElement("h1", {}, "I'm H1 Tag")
+const h2 = React.createElement("h2", {}, "I'm H2 Tag")
+const child = React.createElement("div", {id:"child"}, [h1, h2]);
+const parent = React.createElement("div", {id:"parent"}, child);
+
+
+root.render([heading, parent])
+
+// root.render(parent)
+
+parent.render();
+// JSX.
