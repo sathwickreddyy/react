f435844eabb74810ffc751b02511be3acb466bc0
Sathwick Reddy, Yalla <sathwickreddyy@outlook.com>
2024-09-02 05:35:41 +0530

    Understanding Props to react component

--- food-ordering/App.js
+++ food-ordering/App.js
@@ -1,33 +1,61 @@
 import React from "react";
-import ReactDOM from "react-dom";
-
+import ReactDOM from "react-dom/client";
+import logo from "./public/logo.png";
 
 const Header = () => {
     return (
-        <div className="header">
-            <div className="logo-container">
-                <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pngwing.com%2Fen%2Fsearch%3Fq%3Dfood%2Blogo&psig=AOvVaw0Dixoj1quapfdcJkJlfv5-&ust=1725314565664000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCPjwmunfoogDFQAAAAAdAAAAABA7"/>
+        <div className='header'>
+            <div className='logo-container'>
+                <img className='logo' src={logo} alt='logo' />
             </div>
-            <div className="nav-items">
+            <div className='nav-items'>
                 <ul>
                     <li>Home</li>
-                    <li>About</li>
+                    <li>About Us</li>
                     <li>Contact</li>
                     <li>Cart</li>
                 </ul>
             </div>
         </div>
-    )
-}
+    );
+};
 
+const RestroCard = (props) => {
+    return (
+        <div className='restro-card'>
+            <img
+                className='restro-logo'
+                src='https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/hrn5r5ghcl7m3piog5iy'
+                alt='restaurant'
+            />
+            <h3>{props.resName}</h3>
+            <h4>{props.cuisine}</h4>
+            <h4>4.3</h4>
+            <h4>3.8 mins</h4>
+        </div>
+    );
+};
+
+const Body = () => {
+    return (
+        <div className='body'>
+            <div className='search'>Search</div>
+            <div className='restro-container'>
+                <RestroCard resName='Meghana Foods' cuisine='Biryani, North Indian, Asian' />
+                <RestroCard resName='KFC' cuisine='American, FastFood, Burger' />
+            </div>
+        </div>
+    );
+};
 
 const AppLayout = () => {
     return (
-        <div className="app">
-
+        <div className='app'>
+            <Header />
+            <Body />
         </div>
-    )
+    );
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<AppLayout/>)
+root.render(<AppLayout />);
