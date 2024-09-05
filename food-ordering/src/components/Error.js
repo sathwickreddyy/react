2328d66f7d59b03403ef939191ddb61328fdb391
Sathwick Reddy, Yalla <sathwickreddyy@outlook.com>
2024-09-06 01:25:23 +0530

    Adding useRouterError Hook to Error Component

--- food-ordering/src/components/Error.js
+++ food-ordering/src/components/Error.js
@@ -1,10 +1,16 @@
 import React from "react";
+import { useRouteError } from "react-router-dom";
 
 const Error = () => {
+    const error = useRouteError();
+    console.error("An error occurred:", error);
     return (
         <div>
             <h1>Error Dude</h1>
             <h1>OOPs! Something went wrong</h1>
+            <h3>
+                {error.status}:{error.statusText}
+            </h3>
         </div>
     );
 };
