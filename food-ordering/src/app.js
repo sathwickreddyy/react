8e83285596842118b86c5a568c3aa21030589232
Sathwick Reddy, Yalla <sathwickreddyy@outlook.com>
2024-09-06 01:22:14 +0530

    Adding ErrorElement

--- food-ordering/src/app.js
+++ food-ordering/src/app.js
@@ -4,6 +4,7 @@ import Header from "./components/Header";
 import Body from "./components/Body";
 import About from "./components/About";
 import Contact from "./components/Contact";
+import Error from "./components/Error";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const AppLayout = () => {
@@ -19,6 +20,7 @@ const appRouter = createBrowserRouter([
     {
         path: "/",
         element: <AppLayout />,
+        errorElement: <Error />,
     },
     {
         path: "/about",
---
+++ food-ordering/src/components/Error.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Error = () => {
+    return (
+        <div>
+            <h1>Error Dude</h1>
+            <h1>OOPs! Something went wrong</h1>
+        </div>
+    );
+};
+
+export default Error;
