edb9fcb4034e48ea663d0678a6ade223d5e1664f
Sathwick Reddy, Yalla <sathwickreddyy@outlook.com>
2024-09-06 00:16:54 +0530

    Adding Searching logic

--- food-ordering/src/components/Body.js
+++ food-ordering/src/components/Body.js
@@ -7,6 +7,9 @@ const Body = () => {
     // useState function returns a state variable in a list..
     // first param is the name of the variable, second is the function to modify the variable.
     const [listOfRestaurentData, setListOfRestaurent] = useState([]); // equivalent to let listOfRestro=[];
+    const [filteredRestaurents, setFilteredRestaurents] = useState([]); // equivalent
+
+    const [searchText, setSearchText] = useState("");
 
     useEffect(() => {
         fetchData();
@@ -14,24 +17,47 @@ const Body = () => {
 
     const fetchData = async () => {
         const data = await fetch(
-            "https://www.swiggy.com/api/seo/getListing?lat=12.970195219010778&lng=77.74050557707224&isDineoutCollection=false",
+            "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.970195219010778&lng=77.74050557707223&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING",
         ); // browser function which returns a promise.
 
         const json = await data.json();
         // console.log(json);
-        const rests = json?.data?.success?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants;
-        console.log(rests);
+        const rests = json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants;
+        console.log("Restaurents Data Fetched ", rests);
         setListOfRestaurent(rests);
+        setFilteredRestaurents(rests);
     };
 
-    // Conditional Rendering.
-    if (listOfRestaurentData.length === 0) {
-        return <Shimmer />;
-    }
-
-    return (
+    return listOfRestaurentData.length === 0 ? (
+        <Shimmer />
+    ) : (
         <div className='body'>
             <div className='filter'>
+                <div className='search'>
+                    <input
+                        type='text'
+                        className='search-box'
+                        value={searchText}
+                        onChange={(event) => {
+                            setSearchText(event.target.value);
+                        }}
+                    />
+                    <button
+                        onClick={() => {
+                            // Filter restaurants and update the UI.
+
+                            const filteredRestaurents = listOfRestaurentData.filter((resInfo) =>
+                                resInfo.info.name.toLowerCase().includes(searchText.toLowerCase()),
+                            );
+                            console.log(filteredRestaurents);
+                            if (filteredRestaurents.length > 0) {
+                                setFilteredRestaurents(filteredRestaurents);
+                            }
+                        }}
+                    >
+                        Search
+                    </button>
+                </div>
                 <button
                     className='filter-btn'
                     onClick={() => {
@@ -43,7 +69,7 @@ const Body = () => {
                 </button>
             </div>
             <div className='restro-container'>
-                {listOfRestaurentData.map((restroInfo) => (
+                {filteredRestaurents.map((restroInfo) => (
                     <RestroCard key={restroInfo.info.id} resInfo={restroInfo} />
                 ))}
             </div>
--- food-ordering/src/components/Header.js
+++ food-ordering/src/components/Header.js
@@ -1,6 +1,9 @@
 import logo from "../../public/logo.png";
+import { useState } from "react";
 
 const Header = () => {
+    const [btnName, setBtnName] = useState("Login");
+
     return (
         <div className='header'>
             <div className='logo-container'>
@@ -12,6 +15,15 @@ const Header = () => {
                     <li>About Us</li>
                     <li>Contact</li>
                     <li>Cart</li>
+                    <button
+                        className='login'
+                        onClick={() => {
+                            btnName === "Login" ? setBtnName("Logout") : setBtnName("Login");
+                            console.log(btnName);
+                        }}
+                    >
+                        {btnName}
+                    </button>
                 </ul>
             </div>
         </div>
