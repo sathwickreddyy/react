976ce0a549f6165b202e5eb64faca53bd4878579
Sathwick Reddy, Yalla <sathwickreddyy@outlook.com>
2024-09-06 01:33:41 +0530

    Understanding appRouter Children : Outlet of React-Router-Dom usage

--- food-ordering/src/app.js
+++ food-ordering/src/app.js
@@ -5,13 +5,19 @@ import Body from "./components/Body";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Error from "./components/Error";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 
 const AppLayout = () => {
     return (
         <div className='app'>
             <Header />
-            <Body />
+            <Outlet /> {/** This will be replaced by childs according to path */}
+            {/** if path is / */}
+            {/* <Body /> */}
+            {/** if path is /about */}
+            {/* <About /> */}
+            {/** if path is /contact */}
+            {/* <Contact /> */}
         </div>
     );
 };
@@ -20,15 +26,21 @@ const appRouter = createBrowserRouter([
     {
         path: "/",
         element: <AppLayout />,
+        children: [
+            {
+                path: "/",
+                element: <Body />,
+            },
+            {
+                path: "/about",
+                element: <About />,
+            },
+            {
+                path: "/contact",
+                element: <Contact />,
+            },
+        ],
         errorElement: <Error />,
-    },
-    {
-        path: "/about",
-        element: <About />,
-    },
-    {
-        path: "/contact",
-        element: <Contact />,
     },
 ]);
 
