0b71d69a466926b8fabe20f60746826ab1bcfdaf
Sathwick Reddy, Yalla <sathwickreddyy@outlook.com>
2024-09-06 21:47:20 +0530

    Updating state variables in React Class Components

--- food-ordering/src/components/About.js
+++ food-ordering/src/components/About.js
@@ -1,12 +1,10 @@
 import React from "react";
-import User from "./class-components/User";
 import UserClass from "./class-components/UserClass";
 
 const About = () => {
     return (
         <div>
             <h1>About Page</h1>
-            <User />
             <UserClass name={"Sathwick Reddy, Y"} location={"Jagtial Bidda"} />
         </div>
     );
--- food-ordering/src/components/class-components/UserClass.js
+++ food-ordering/src/components/class-components/UserClass.js
@@ -6,13 +6,14 @@ class UserClass extends React.Component {
 
         this.state = {
             count: 0,
+            count2: 0,
         };
     }
 
     render() {
         const { name, location } = this.props;
 
-        const { count } = this.state;
+        const { count, count2 } = this.state;
 
         return (
             <div className='user-card'>
@@ -20,6 +21,19 @@ class UserClass extends React.Component {
                 <h2>Location: {location}</h2>
                 <h3>Contact: sathwick@outlook.in</h3>
                 <h4>Count : {count}</h4>
+                <h4>Count2 : {count2}</h4>
+                <button
+                    onClick={() => {
+                        // We should never update variables directly
+                        // To update we need to use as below
+                        this.setState({
+                            count: this.state.count + 1,
+                            count2: this.state.count2 + 2,
+                        });
+                    }}
+                >
+                    Update Counts
+                </button>
             </div>
         );
     }
