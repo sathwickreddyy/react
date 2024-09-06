e7f4f7edc0d747551f39010f5a6a668c03087eba
Sathwick Reddy, Yalla <sathwickreddyy@outlook.com>
2024-09-06 21:16:15 +0530

    Adding state variables in a Class Component

--- food-ordering/src/components/class-components/UserClass.js
+++ food-ordering/src/components/class-components/UserClass.js
@@ -3,16 +3,23 @@ import React from "react";
 class UserClass extends React.Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            count: 0,
+        };
     }
 
     render() {
         const { name, location } = this.props;
+
+        const { count } = this.state;
 
         return (
             <div className='user-card'>
                 <h2>Name: {name}</h2>
                 <h2>Location: {location}</h2>
                 <h3>Contact: sathwick@outlook.in</h3>
+                <h4>Count : {count}</h4>
             </div>
         );
     }
