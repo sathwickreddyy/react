e078acc38258e5aec1d70669c018c0ca05ce159d
Sathwick reddy <sathwickreddyy@outlook.com>
2024-09-14 17:10:54 +0530

    Configured Taildwind CSS in the app

--- netflix-gpt/src/App.js
+++ netflix-gpt/src/App.js
@@ -1,7 +1,5 @@
-import "./App.css";
-
 function App() {
-    return <div className='App'>Hello EveryOne, Lets Build the Netflix GPT</div>;
+    return <div className='text-3xl font-bold text-green-500'>Hello EveryOne, Lets Build the Netflix GPT</div>;
 }
 
 export default App;
