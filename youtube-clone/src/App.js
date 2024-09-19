9520eaf97b1b539df519b28f553c5d2bd800dfc0
Sathwick reddy <sathwickreddyy@outlook.com>
2024-09-19 17:11:12 +0530

    Initialise Project

---
+++ youtube-clone/src/App.js
@@ -0,0 +1,21 @@
+/**
+ * Home
+ *  Header,
+ *
+ *    Body
+ *     Side Bar,
+ *     MainContainer
+ *        ButtonList
+ *        VideoContainer
+ *          VideoCard
+ */
+
+function App() {
+    return (
+        <div className='App'>
+            <div>Bunny Reddy</div>
+        </div>
+    );
+}
+
+export default App;
